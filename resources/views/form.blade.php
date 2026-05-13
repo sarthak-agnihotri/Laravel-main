50236ab258903ff8cc50cb3fb5d1ef282c4edaf4
Sarthak <sarthak0144@gmail.com>
2026-05-13 10:53:56 +0530

    Update form blade

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -18,10 +18,10 @@
             </div>
         @endif
         <label for="name">Name:</label>
-        <input type="text" id="name" name="name">
+        <input type="text" id="name" name="name" value="{{ old('name') }}">
         <br><br>
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email">
+        <input type="email" id="email" name="email" value="{{ old('email') }}">
         <br><br>
         <button type="submit">Submit</button>
     </form>
