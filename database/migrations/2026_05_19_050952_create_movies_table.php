821f0f0ade8db9efdb7c23f4afb3180e9258bc66
sarthak-agnihotri <sarthak0144@gmail.com>
2026-05-19 10:52:02 +0530

    update migration

--- database/migrations/2026_05_19_050952_create_movies_table.php
+++ database/migrations/2026_05_19_050952_create_movies_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('movie_name')->unique();
+            $table->integer('rating');
+            $table->text('description')->nullable();
+            $table->date('release_date')->nullable();
+            $table->string('category');
         });
     }
 
